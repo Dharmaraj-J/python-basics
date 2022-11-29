2f44747fc38b4a70df0a85575e5169a90d376c8a
Dharmarj-J <dharmaraj200303@gmail.com>
2022-11-28 21:31:22 -1200

    first commit

---
+++ reversing.py
@@ -0,0 +1,17 @@
+list = ['ram','mathava','manoj','karan']
+
+reverse = list[::-1]
+
+print(reverse) # reverse a list
+
+value = range(20)
+
+for values in reversed(value):
+
+    print(values) #reversing numbers
+
+
+
+
+
+
