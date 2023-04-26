fe409d288a0d809d6648582e3bc6d9babbff18c1
Dharmarj-J <dharmaraj200303@gmail.com>
2023-04-25 22:03:48 -1200

    updated

--- decorators/multipledec.py
+++ decorators/multipledec.py
@@ -1,23 +1,23 @@
-def split(ref):
-    def process1():
-        data = ref()
-        return data.split()
-
-    return process1
+# Firstly the inner decorator will work and then the outer decorator work
 
 
-def concatinate(ref):
-    def process2():
-        word = ref()
-        return word + ['how','are','you']
-    return process2
-
-
-@concatinate
-@split
-def fun():
-    return "hi friends"
-
-print(fun())
+def decor2(func):
+    def inner():
+        x = func()
+        return x * x
+    return inner
+ 
+def decor1(func):
+    def inner():
+        x = func()
+        return 2 * x
+    return inner
+ 
+@decor2
+@decor1
+def num():
+    return 10
+ 
+print(num())
 
 
